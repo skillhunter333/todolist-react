a5bcf56d2609c91b0d9ac04f4677bc855cb01b89
boydlakers15 <jboyd115@icloud.com>
2023-03-14 22:01:36 +0100

    Two diffrent styles

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ const TodoList = () => {
 
   const addTodo = (event) => {
     event.preventDefault();
-    const todo = event.target.todo.value;
+    const todo = {
+      title: event.target.title.value,
+      done: false,
+    };
     setTodos([...todos, todo]);
     event.target.reset();
   };
@@ -26,19 +29,50 @@ const TodoList = () => {
     setTodos(newTodos);
   };
 
+  const editTodo = (index, newTitle) => {
+    const newTodos = [...todos];
+    newTodos[index].title = newTitle;
+    setTodos(newTodos);
+  };
+
+  const toggleDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].done = !newTodos[index].done;
+    setTodos(newTodos);
+  };
+
   return (
     
     <div className='TodoWrapper'>
       <h1>Todo List</h1>
       <form onSubmit={addTodo} className="TodoForm">
-        <input className="todo-input" type="text" name="todo" placeholder="Add a todo" />
+        <p>Push button to add to the list</p>
+        {/* <input className="todo-input" type="text" name="todo" placeholder="Add a todo" /> */}
         <button className='todo-btn' type="submit">Add</button>
       </form>
       <ul >
         {todos.map((todo, index) => (
           <li className='Todo' key={index}>
-            {todo}
+             <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => toggleDone(index)}
+            />
+            <span style={{ textDecoration: todo.done ? "line-through" : "" }}>
+            {todo.title}
+            <p className='input'></p></span>
+            
             <button className='todo-delete' onClick={() => deleteTodo(index)}>Delete</button>
+            <button className='todo-edit'
+              onClick={() => {
+                const newTitle = prompt("Enter new title:", todo.title);
+                if (newTitle !== null) {
+                  editTodo(index, newTitle);
+                }
+              }}
+            >
+              Edit
+            </button>
           </li>
         ))}
       </ul>
