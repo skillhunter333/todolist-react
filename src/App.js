0e31a27eac2559cca6e04612d0f73f3a8f673271
boydlakers15 <jboyd115@icloud.com>
2023-03-14 21:26:17 +0100

    Two difffrent kinds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    const todo = event.target.todo.value;
+    setTodos([...todos, todo]);
+    event.target.reset();
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='TodoWrapper'>
+      <h1>Todo List</h1>
+      <form onSubmit={addTodo} className="TodoForm">
+        <input className="todo-input" type="text" name="todo" placeholder="Add a todo" />
+        <button type="submit">Add</button>
+      </form>
+      <ul >
+        {todos.map((todo, index) => (
+          <li className='Todo' key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
