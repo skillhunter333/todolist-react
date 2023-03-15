b195918f0b52210274f1a26d5401198349549a65
boydlakers15 <jboyd115@icloud.com>
2023-03-15 11:06:03 +0100

    Added in a date function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [newDate, setNewDate] = useState("");
 
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -20,9 +21,11 @@ const TodoList = () => {
       const todo = {
         title: newTodo,
         done: false,
+        date: newDate,
       };
       setTodos([...todos, todo]);
       setNewTodo("");
+      setNewDate("");
     }
   };
 
@@ -32,9 +35,10 @@ const TodoList = () => {
     setTodos(newTodos);
   };
 
-  const editTodo = (index, newTitle) => {
+  const editTodo = (index, newTitle, newDate) => {
     const newTodos = [...todos];
     newTodos[index].title = newTitle;
+    newTodos[index].date = newDate;
     setTodos(newTodos);
   };
 
@@ -44,45 +48,52 @@ const TodoList = () => {
     setTodos(newTodos);
   };
 
-
   return (
-    
     <div className='TodoWrapper'>
       <h1>Todo List</h1>
-      <form onSubmit={addTodo} className="TodoForm">
+      <form className="TodoForm" onSubmit={addTodo}>
         <input
-        className='todo-input'
+          className="todo-input"
           type="text"
           name="title"
           placeholder="Add a todo"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
         />
+        <input
+          type="date"
+          name="date"
+          value={newDate}
+          onChange={(e) => setNewDate(e.target.value)}
+        />
         <button className='todo-btn' type="submit">Add</button>
       </form>
-      <ul >
+      <ul className="todo-list">
         {todos.map((todo, index) => (
-          <li className='Todo' key={index}>
-             <input
+          <li key={index} className='Todo'>
+            <input
               type="checkbox"
               checked={todo.done}
               onChange={() => toggleDone(index)}
             />
             <span style={{ textDecoration: todo.done ? "line-through" : "" }}>
-            {todo.title}
-            <p className='input'></p></span>
-            
-            <button className='todo-delete' onClick={() => deleteTodo(index)}>Delete</button>
-            <button className='todo-edit'
-              onClick={() => {
-                const newTitle = prompt("Enter new item:", todo.title);
-                if (newTitle !== null) {
-                  editTodo(index, newTitle);
-                }
-              }}
-            >
-              Edit
-            </button>
+            {todo.title}</span>
+            <span>{todo.date}</span>
+            <div className="buttons-container">
+              <button className='todo-delete' onClick={() => deleteTodo(index)}>Delete</button>
+              <button
+              className='todo-edit'
+                onClick={() => {
+                  const newTitle = prompt("Enter new title:", todo.title);
+                  const newDate = prompt("Enter new due date:", todo.date);
+                  if (newTitle !== null) {
+                    editTodo(index, newTitle, newDate);
+                  }
+                }}
+              >
+                Edit
+              </button>
+            </div>
           </li>
         ))}
       </ul>
